206cff128eb44bd8330378fa9e95d37d85539cb8
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-05-19 12:01:13 -0700

    Started CookieTester

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    var readCookie = document.getElementById("read-cookie");
+    var deleteCookie = document.getElementById("delete-cookie");
+    var createCookie = document.getElementById("create-cookie");
+    readCookie.onclick = readCookieData;
+    deleteCookie.onclick = deleteCookieData;
+    createCookie.onclick = createCookieData;
+};
+function readCookieData() {
+}
+function deleteCookieData() {
+}
+function createCookieData() {
+}
---
+++ ts/main.ts
@@ -0,0 +1,24 @@
+window.onload = function(){
+    let readCookie = document.getElementById("read-cookie");
+    let deleteCookie = document.getElementById("delete-cookie");
+    let createCookie = document.getElementById("create-cookie");
+
+
+    readCookie.onclick = readCookieData;
+    deleteCookie.onclick = deleteCookieData;
+    createCookie.onclick = createCookieData;
+
+    
+}
+
+function readCookieData(){
+
+}
+
+function deleteCookieData(){
+
+}
+
+function createCookieData(){
+
+}
